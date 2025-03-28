ca3dc8f5b9ebace88499b0406e507ff63ffe84a6
dragenlord <K.Malkin2009@yandex.ru>
2025-03-29 00:10:53 +0300

    //

---
+++ src/main/java/com/example/interview/config/Telegram_config.java
@@ -0,0 +1,4 @@
+package com.example.interview.config;
+
+public class Telegram_config {
+}
---
+++ src/main/java/com/example/interview/telegram/Bot.java
@@ -0,0 +1,9 @@
+package com.example.interview.telegram;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+@Component
+public class Bot  extends TelegramLongPollingBot {
+
+}
